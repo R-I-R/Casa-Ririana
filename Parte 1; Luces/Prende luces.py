7d4d08fcc8e803803966ba4a7ef9099c29b16fe9
Esteban <esteban.naranjo.rayo@gmail.com>
2017-10-19 05:13:54 -0300

    Delete Prende luces.py

--- Parte 1; Luces/Prende luces.py
+++
@@ -1,5 +0,0 @@
-import serial, time#importamos las librerias
-arduino = serial.Serial("COM4", 9600)#cambiar COM4 por el COM en el que esta tu arduino
-time.sleep(2)#espera 2 segundos para conectarse con el arduino
-arduino.write(b'p')#le envia por serial una p, y si vemos el codigo del arduino sabemos que con eso prende
-arduino.close()#cerramos la comunicacion del serial
