a5cc4c392f53533b8f9bd4623f9fa632b11330f5
Esteban <esteban.naranjo.rayo@gmail.com>
2017-10-19 02:59:20 -0300

    Update Apaga luces.py

--- Parte 1; Luces/Apaga luces.py
+++ Parte 1; Luces/Apaga luces.py
@@ -1,5 +1,5 @@
-import serial, time
-arduino = serial.Serial("COM4", 9600)
-time.sleep(2)
-arduino.write(b'a')
-arduino.close()
+import serial, time#importamos las librerias
+arduino = serial.Serial("COM4", 9600)#cambiar COM4 por el COM en el que esta tu arduino
+time.sleep(2)#espera 2 segundos para conectarse con el arduino
+arduino.write(b'a')#le envia por serial una a, y si vemos el codigo del arduino sabemos que con eso se apaga
+arduino.close()#cerramos la comunicacion del serial
